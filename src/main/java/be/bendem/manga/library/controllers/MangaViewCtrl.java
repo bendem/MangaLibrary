515e166b877d50e678c2960c35fb47d984570840
bendem <online@bendem.be>
2015-07-16 20:48:02 +0200

    Reformat fxml and mess around with sizings

--- src/main/java/be/bendem/manga/library/controllers/MangaViewCtrl.java
+++ src/main/java/be/bendem/manga/library/controllers/MangaViewCtrl.java
@@ -3,18 +3,22 @@ package be.bendem.manga.library.controllers;
 import be.bendem.manga.library.MangaLibrary;
 import be.bendem.manga.library.utils.Log;
 import be.bendem.manga.library.utils.NumberUtil;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ResourceBundle;
 
-public class MangaViewCtrl {
+public class MangaViewCtrl implements Initializable {
 
     @FXML private VBox imageContainer;
     @FXML private ImageView image;
@@ -23,6 +27,11 @@ public class MangaViewCtrl {
 
     public MangaViewCtrl(MangaLibrary app) {
         this.app = app;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        imageContainer.autosize();
     }
 
     public void setChapter(String manga, String chapter) {
@@ -45,17 +54,28 @@ public class MangaViewCtrl {
                 .sorted(NumberUtil::compare)
                 .findFirst()
                 .get();
-            Log.info(path.toString());
+            //Log.info(path.toString());
             is = Files.newInputStream(path);
         } catch(IOException e) {
             throw new RuntimeException(e);
         }
         // TODO Loading bar using img.progressProperty()!
+        Log.debug("w" + imageContainer.getWidth() + " h" + imageContainer.getHeight());
+        Log.debug(imageContainer.getBoundsInLocal().toString());
+
         image.setSmooth(true);
         image.setPreserveRatio(true);
         image.setFitHeight(imageContainer.getHeight());
         image.setFitWidth(imageContainer.getWidth());
         image.setImage(new Image(is));
+    }
+
+    public void onPrevAction(ActionEvent event) {
+        // TODO
+    }
+
+    public void onNextAction(ActionEvent event) {
+        // TODO
     }
 
 }
